1829dbe2187428fa2a5a2be0da2a530ec05bb825
beadevbr <beadevbr@gmail.com>
2024-02-05 23:21:36 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,19 @@
+// btnLeiaMais.addEventListener('click', () => {})
+
+// function leiaMais() {
+//   var pontos = document.getElementById('pontos')
+//   var maisTextos = document.getElementById('mais')
+//   var btnLeiaMais = document.getElementById('btnLeiaMais')
+
+//   if (pontos.style.display === 'none') {
+//     pontos.style.display = 'inline'
+//     maisTextos.style.display = 'none'
+//     btnLeiaMais.innerHTML = 'view more'
+//     btnLeiaMais.style.marginTop = '-9px'
+//   } else {
+//     pontos.style.display = 'none'
+//     maisTextos.style.display = 'inline'
+//     btnLeiaMais.innerHTML = 'show less'
+//     btnLeiaMais.style.marginTop = '9px'
+//   }
+// }
